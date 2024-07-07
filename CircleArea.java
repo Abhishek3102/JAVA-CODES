4e933d1bda2322a20ae03f6af66dadf79870e106
Abhishe16 <ankushpan694@gmail.com>
2024-07-07 16:07:08 +0530

    area code added

---
+++ CircleArea.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CircleArea {
+public static void main(String args[]) {
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter radius of Circle");
+double r = sc.nextDouble();
+double area = 3.14*r*r;
+System.out.println("The area of circle is: "+area);
+         }
+    }
+    
